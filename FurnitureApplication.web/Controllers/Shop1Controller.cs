ea3f1e355fc0db803636ff261d377ed34d233385
Ayaz-Riaz <ayazbright.bright@gmail.com>
2018-11-22 20:50:39 +0500

    change in email for checkout process

--- FurnitureApplication.web/Controllers/Shop1Controller.cs
+++ FurnitureApplication.web/Controllers/Shop1Controller.cs
@@ -1,11 +1,14 @@
 ﻿using FurnitureApplication.Entities;
 using FurnitureApplication.Services;
+using FurnitureApplication.web.Models;
 using FurnitureApplication.web.ViewModels;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
 using System.Web;
 using System.Web.Mvc;
 
@@ -115,12 +118,13 @@ namespace FurnitureApplication.web.Controllers
 
             return View(model);
         }
-        public JsonResult PlaceOrder(string productIDs)
+        public JsonResult PlaceOrder(CheckoutViewModel model, string productIDs)
         {
+            model.User = UserManager.FindById(User.Identity.GetUserId());
             JsonResult result = new JsonResult();
             result.JsonRequestBehavior = JsonRequestBehavior.AllowGet;
 
-            if(!string.IsNullOrEmpty (productIDs))
+            if (!string.IsNullOrEmpty(productIDs))
             {
                 var productQuantities = productIDs.Split('-').Select(x => int.Parse(x)).ToList();
                 var boughtProducts = ProductsServices.Instance.GetProducts(productQuantities.Distinct().ToList());
@@ -132,11 +136,38 @@ namespace FurnitureApplication.web.Controllers
                 newOrder.TotalAmount = boughtProducts.Sum(x => x.Price * productQuantities.Where(productID => productID == x.ID).Count());
 
                 newOrder.OrderItems = new List<OrderItem>();
-                newOrder.OrderItems.AddRange(boughtProducts.Select(x=> new OrderItem() { ProductID = x.ID, Quantity = productQuantities.Where(productID => productID == x.ID).Count() }));
+                newOrder.OrderItems.AddRange(boughtProducts.Select(x => new OrderItem() { ProductID = x.ID, Quantity = productQuantities.Where(productID => productID == x.ID).Count() }));
 
+           
                 var rowsEffected = ShopService.Instance.SaveOrder(newOrder);
-            
                 result.Data = new { Success = true, Rows = rowsEffected };
+
+                //*********Email sending code
+
+                var fromEmail = new MailAddress("rastgroup1@gmail.com", "Rast Group");
+                var toEmail = new MailAddress(model.User.Email);
+                var fromEmailPassword = "ayaz123456"; //replace with actual Password
+
+                string subject = "Order";
+                string body = "Your Order Placed successfully";
+                
+                var smtp = new SmtpClient
+                {
+                    Host = "smtp.gmail.com",
+                    Port = 587,
+                    EnableSsl = true,
+                    DeliveryMethod = SmtpDeliveryMethod.Network,
+                    UseDefaultCredentials = false,
+                    Credentials = new NetworkCredential(fromEmail.Address, fromEmailPassword)
+                };
+                using (var message = new MailMessage(fromEmail, toEmail)
+                {
+
+                    Subject = subject,
+                    Body = body,
+                    IsBodyHtml = true
+                })
+                    smtp.Send(message);
             }
             else
             {
